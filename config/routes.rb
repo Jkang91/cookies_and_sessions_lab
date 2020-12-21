58e6d9ef8c103b06c9e1a37980df59cc41e2fc9e
June Kang <Jkang34@gmail.com>
2020-12-21 12:35:35 -0500

    cookies sessions lab

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get "products", to: "products#index", as: "products"
+  root 'products#index'
+  post '/', to: "products#add_product"
 end
